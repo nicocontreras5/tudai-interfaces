29c9a76bd91cafa982489f60ed8a2e322e1ecca6
Nico Contreras <nicocontreras538@gmail.com>
2020-10-30 04:39:35 -0300

    bug rueda

--- entregable3/js/calendario.js
+++ entregable3/js/calendario.js
@@ -3,7 +3,7 @@ function loadPage() {
   
   let eventos = document.getElementsByClassName("accordion");
 
-  console.log(height);
+
   for (let i = 0; i < eventos.length; i++) {
     eventos[i].addEventListener("click", function() {
       this.classList.toggle("active");
