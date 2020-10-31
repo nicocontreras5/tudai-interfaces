f6accc9d26d34cee9723fbb939ec87615b77390d
Nico Contreras <nicocontreras538@gmail.com>
2020-10-31 17:27:36 -0300

    entrega final

--- entregable3/js/scroll.js
+++ entregable3/js/scroll.js
@@ -1,33 +1,46 @@
 
 function loadPage() {
-  
+
     function scroll() {
 
         
         let cards = document.querySelector(".cards");
-
+        let car = document.querySelector(".car");
+        
+        
         
         let escenas = document.querySelector(".escenas");
+        let mover = 0;
+        // CAR
         
-        if (window.scrollY >= 193) {
+        
+        // ESCENAS
+        
+        if (window.scrollY >= 300) {
             escenas.classList.add("entrada-escenas");
         } else {
             
             escenas.classList.remove("entrada-escenas");
         }
-        console.log(window.scrollY );
-
-        if (window.scrollY >= 870) {
+        
+        // CARDS
+        if (window.scrollY >= 920) {
             
-           cards.classList.add("entrada-cards");
+            cards.classList.add("entrada-cards");
         } else {
             
             cards.classList.remove("entrada-cards");
         }
+        mover = window.scrollY*2.5;
+        car.style.marginRight  =  mover   + "px";
+        
+        
     }
-
+    
+    
+       
     window.addEventListener("scroll", scroll);
-  
+    
     
     
   }document.addEventListener("DOMContentLoaded", loadPage);
